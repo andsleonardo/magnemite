7988ce695f3a70ded42e4f5031a9457ed71ad87f
Anderson Leonardo <andsnleo@gmail.com>
2021-03-06 11:55:09 -0300

    Create customers table

---
+++ priv/repo/migrations/20210306111555_create_customers.exs
@@ -0,0 +1,14 @@
+defmodule Magnemite.Repo.Migrations.CreateCustomers do
+  use Ecto.Migration
+
+  def change do
+    create table(:customers, primary_key: false) do
+      add :id, :uuid, primary_key: true, null: false
+      add :birth_date, :date
+      add :cpf, :string, null: false
+      add :email, :string
+      add :gender, {:array, :string}
+      add :name, :string
+    end
+  end
+end
