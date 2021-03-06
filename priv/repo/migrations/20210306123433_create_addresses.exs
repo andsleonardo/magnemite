e777fc4a4ebcda9f94ffac49c240640e8d509a5b
Anderson Leonardo <andsnleo@gmail.com>
2021-03-06 12:07:27 -0300

    Create addresses table

---
+++ priv/repo/migrations/20210306123433_create_addresses.exs
@@ -0,0 +1,14 @@
+defmodule Magnemite.Repo.Migrations.CreateAddresses do
+  use Ecto.Migration
+
+  def change do
+    create table(:addresses, primary_key: false) do
+      add :id, :uuid, primary_key: true, null: false
+      add :city, :string
+      add :country, :string
+      add :state, :string
+
+      add :customer_id, references(:customers, type: :uuid, on_delete: :delete_all), null: false
+    end
+  end
+end
