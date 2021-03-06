6f7d1ff1328c5f45b7bd15b2a398762215aecde2
Anderson Leonardo <andsnleo@gmail.com>
2021-03-06 12:10:33 -0300

    Create account_opening_requests table

---
+++ priv/repo/migrations/20210306131537_create_account_opening_requests.exs
@@ -0,0 +1,12 @@
+defmodule Magnemite.Repo.Migrations.CreateAccountOpeningRequests do
+  use Ecto.Migration
+
+  def change do
+    create table(:account_opening_requests, primary_key: false) do
+      add :id, :uuid, primary_key: true, null: false
+      add :status, :string, null: false
+
+      add :customer_id, references(:customers, type: :uuid, on_delete: :delete_all), null: false
+    end
+  end
+end
