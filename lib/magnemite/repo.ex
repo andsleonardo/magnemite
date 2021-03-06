281ad99abf12803457371d143184e669f9f6361f
Anderson Leonardo <andsnleo@gmail.com>
2021-03-06 16:52:01 -0300

    Add helper functions to Magnemite.Repo

--- lib/magnemite/repo.ex
+++ lib/magnemite/repo.ex
@@ -1,5 +1,13 @@
 defmodule Magnemite.Repo do
+  @moduledoc false
+
   use Ecto.Repo,
     otp_app: :magnemite,
     adapter: Ecto.Adapters.Postgres
+
+  def handle_operation_result({:ok, record} = output), do: output
+
+  def handle_operation_result({:error, %Ecto.Changeset{} = changeset}) do
+    {:error, Magnemite.Changeset.errors_on(changeset)}
+  end
 end
---
+++ test/magnemite/repo_test.exs
@@ -0,0 +1,29 @@
+defmodule Magnemite.RepoTest do
+  use ExUnit.Case, async: true
+
+  alias Magnemite.Repo
+
+  describe "handle_operation_result/1" do
+    test "returns the successful output when given an :ok tuple" do
+      output = {:ok, %{field: "value"}}
+
+      assert ^output = Repo.handle_operation_result(output)
+    end
+
+    test "returns transformed changeset errors when given an :error tuple with a changeset" do
+      changeset = %Ecto.Changeset{
+        types: [{:field1, :integer}, {:field2, :string}],
+        errors: [
+          field1: {"can't be blank", []},
+          field2: {"is invalid", []}
+        ]
+      }
+
+      assert {:error,
+              %{
+                field1: ["can't be blank"],
+                field2: ["is invalid"]
+              }} = Repo.handle_operation_result({:error, changeset})
+    end
+  end
+end
