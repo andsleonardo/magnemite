b1a2cd745ac02371478f4a31a7a0d0e0d519b496
Anderson Leonardo <andsnleo@gmail.com>
2021-03-07 15:44:23 -0300

    Refactor FallbackController, add new clause

--- lib/magnemite_web/controllers/api/fallback_controller.ex
+++ lib/magnemite_web/controllers/api/fallback_controller.ex
@@ -2,9 +2,23 @@ defmodule MagnemiteWeb.Api.FallbackController do
   use MagnemiteWeb, :controller
 
   def call(conn, {:error, :changeset, errors}) do
+    bad_request(conn, errors)
+  end
+
+  def call(conn, {:error, error}) do
+    bad_request(conn, [translate_error(error)])
+  end
+
+  defp bad_request(conn, errors) do
     conn
     |> put_status(:bad_request)
     |> json(render_errors(errors))
+  end
+
+  defp translate_error(error) when is_atom(error) do
+    ~r/_/
+    |> Regex.replace(Atom.to_string(error), " ")
+    |> String.capitalize()
   end
 
   defp render_errors(errors) do
--- test/magnemite_web/controllers/api/fallback_controller_test.exs
+++ test/magnemite_web/controllers/api/fallback_controller_test.exs
@@ -18,4 +18,18 @@ defmodule MagnemiteWeb.Api.FallbackControllerTest do
              } = parsed_resp_body!(conn)
     end
   end
+
+  describe "call/1 with {:error, error}" do
+    test "puts a 422 status to conn and renders errors", %{
+      conn: conn
+    } do
+      error = {:error, :invalid_referral_code}
+
+      assert %Plug.Conn{status: 400} = conn = FallbackController.call(conn, error)
+
+      assert %{
+               "errors" => ["Invalid referral code"]
+             } = parsed_resp_body!(conn)
+    end
+  end
 end
