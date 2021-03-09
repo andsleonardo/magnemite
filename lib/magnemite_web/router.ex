f6172d4d72d6b5c21f6b14aaa9412f0e9f67584d
Anderson Leonardo <andsnleo@gmail.com>
2021-03-09 06:50:26 -0300

    Add signin and singup routes, use auth pipeline

--- lib/magnemite_web/router.ex
+++ lib/magnemite_web/router.ex
@@ -5,11 +5,26 @@ defmodule MagnemiteWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :api_auth do
+    plug :accepts, ["json"]
+    plug MagnemiteWeb.AuthPipeline
+  end
+
+  scope "/api", MagnemiteWeb.Api do
+    pipe_through :api_auth
+
+    scope "/v1", V1 do
+      resources "/accounts", AccountController, only: [:create]
+      resources "/referrals", ReferralController, only: [:index]
+    end
+  end
+
   scope "/api", MagnemiteWeb.Api do
     pipe_through :api
 
     scope "/v1", V1 do
-      resources "/accounts", AccountController, only: [:create]
+      post "/users/sign_up", UserController, :sign_up
+      post "/users/sign_in", UserController, :sign_in
     end
   end
 end
