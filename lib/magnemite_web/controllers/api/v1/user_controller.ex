4df5b9ea0904aa24521c3ca5291139e4c59cae1a
Anderson Leonardo <andsnleo@gmail.com>
2021-03-09 06:51:04 -0300

    Create user controller with signin and signup actions

---
+++ lib/magnemite_web/controllers/api/v1/user_controller.ex
@@ -0,0 +1,38 @@
+defmodule MagnemiteWeb.Api.V1.UserController do
+  @moduledoc false
+
+  use MagnemiteWeb, :controller
+
+  alias __MODULE__.SignUpParams
+  alias MagnemiteWeb.Api.V1.UserView
+
+  import MagnemiteWeb.Api.Parameterizer
+
+  action_fallback MagnemiteWeb.Api.FallbackController
+
+  @doc """
+  Registers an user.
+
+  * Method: POST
+  * Endpoint: /api/v1/users/sign_up
+  """
+  def sign_up(conn, params) do
+    with {:ok, params} <- permit_and_require_params(params, &SignUpParams.changeset/1),
+         {:ok, user} <- Magnemite.sign_user_up(params.username, params.password) do
+      json(conn, UserView.render("sign_up.json", %{user: user}))
+    end
+  end
+
+  @doc """
+  Signs an user in, generating a token.
+
+  * Method: POST
+  * Endpoint: /api/v1/users/sign_in
+  """
+  def sign_in(conn, params) do
+    with {:ok, params} <- permit_and_require_params(params, &SignUpParams.changeset/1),
+         {:ok, user} <- Magnemite.sign_user_in(params.username, params.password) do
+      json(conn, UserView.render("sign_in.json", %{user: user}))
+    end
+  end
+end
---
+++ test/magnemite_web/controllers/api/v1/user_controller_test.exs
@@ -0,0 +1,105 @@
+defmodule MagnemiteWeb.Api.V1.UserControllerTest do
+  use MagnemiteWeb.ConnCase, async: true
+
+  alias Magnemite
+
+  describe "sign_up/2" do
+    @valid_sign_up_params %{
+      username: Faker.Internet.user_name(),
+      password: Ecto.UUID.generate()
+    }
+
+    test "returns 200 and a user object when all required params are given", %{
+      conn: conn
+    } do
+      %{username: username} = @valid_sign_up_params
+
+      conn = sign_user_up(conn, @valid_sign_up_params)
+
+      assert conn.status == 200
+
+      assert %{
+               "id" => _,
+               "username" => ^username
+             } = parsed_resp_body!(conn)
+    end
+
+    test "returns 400 and errors when required params are missing", %{
+      conn: conn
+    } do
+      conn = sign_user_up(conn, %{})
+
+      assert conn.status == 400
+
+      assert %{
+               "errors" => %{
+                 "password" => ["can't be blank"],
+                 "username" => ["can't be blank"]
+               }
+             } = parsed_resp_body!(conn)
+    end
+  end
+
+  describe "sign_in/2" do
+    @valid_sign_in_params %{
+      username: Faker.Internet.user_name(),
+      password: Ecto.UUID.generate()
+    }
+
+    test """
+         returns 200 and a user object when all required params are given
+         and they match an existing user
+         """,
+         %{
+           conn: conn
+         } do
+      {:ok, %{id: user_id, username: username}} =
+        Magnemite.sign_user_up(@valid_sign_in_params.username, @valid_sign_in_params.password)
+
+      conn = sign_user_in(conn, @valid_sign_in_params)
+
+      assert conn.status == 200
+
+      assert %{
+               "id" => ^user_id,
+               "username" => ^username,
+               "token" => _
+             } = parsed_resp_body!(conn)
+    end
+
+    test "returns 400 and errors when required params are missing", %{
+      conn: conn
+    } do
+      conn = sign_user_in(conn, %{})
+
+      assert conn.status == 400
+
+      assert %{
+               "errors" => %{
+                 "password" => ["can't be blank"],
+                 "username" => ["can't be blank"]
+               }
+             } = parsed_resp_body!(conn)
+    end
+
+    test "returns 400 and errors when there's no user matching params", %{
+      conn: conn
+    } do
+      conn = sign_user_in(conn, @valid_sign_in_params)
+
+      assert conn.status == 400
+
+      assert %{
+               "errors" => ["user not found"]
+             } = parsed_resp_body!(conn)
+    end
+
+    defp sign_user_up(conn, params) do
+      post(conn, Routes.user_path(conn, :sign_up, params))
+    end
+
+    defp sign_user_in(conn, params) do
+      post(conn, Routes.user_path(conn, :sign_in, params))
+    end
+  end
+end
