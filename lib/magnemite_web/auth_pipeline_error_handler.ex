7b046222d14349f7376391c495b5cb0627089a8d
Anderson Leonardo <andsnleo@gmail.com>
2021-03-13 13:49:10 -0300

    Improve rendered error from auth pipeline

--- lib/magnemite_web/auth_pipeline_error_handler.ex
+++ lib/magnemite_web/auth_pipeline_error_handler.ex
@@ -2,13 +2,11 @@ defmodule MagnemiteWeb.AuthPipelineErrorHandler do
   @moduledoc false
 
   import Plug.Conn
-  import Phoenix.Controller, only: [json: 2]
 
   @behaviour Guardian.Plug.ErrorHandler
 
   @impl true
   def auth_error(conn, {type, reason}, _opts) do
-    body = json(conn, %{errors: [to_string(type)]})
-    send_resp(conn, 401, body)
+    send_resp(conn, 401, "Unauthorized")
   end
 end
