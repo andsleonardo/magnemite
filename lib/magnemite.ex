429121aa09738e31f2bd9ff80a81114aaa600d37
Anderson Leonardo <andsnleo@gmail.com>
2021-03-13 13:44:01 -0300

    Refactor Magnemite public API

--- lib/magnemite.ex
+++ lib/magnemite.ex
@@ -4,83 +4,66 @@ defmodule Magnemite do
   """
 
   alias __MODULE__.{Accounts, Customers, Identity}
-  alias __MODULE__.Repo
 
   defdelegate sign_user_up(username, password), to: Identity, as: :sign_up
   defdelegate sign_user_in(username, password), to: Identity, as: :sign_in
+  defdelegate get_profile_by_user_id(user_id), to: Customers
 
-  def list_referrees(referrer_id) do
-    with {:ok, referrer} <- Customers.get_profile(referrer_id),
-         {:ok, referrer} <- validate_profile_opened_account(referrer) do
-      referrals =
-        referrer.id
-        |> Accounts.list_complete_account_opening_requests()
-        |> Enum.map(&Customers.build_referree(&1.id, &1.profile.name))
-
-      {:ok, referrals}
-    end
-  end
-
-  defp validate_profile_opened_account(profile) do
-    profile.id
-    |> Accounts.request_account_opening()
-    |> case do
-      {:ok, %{status: :complete}} -> {:ok, profile}
-      _ -> {:error, :incomplete_account_opening_request}
+  def list_referred_accounts(user_id) do
+    with {:ok, referrer} <- Customers.get_profile_by_user_id(user_id),
+         {:ok, account_opening_request} <-
+           Accounts.get_account_opening_request_by_profile_id(referrer.id) do
+      if Accounts.complete_account_opening_request?(account_opening_request) do
+        {:ok, Accounts.list_complete_referred_accounts(referrer.id)}
+      else
+        {:error, :incomplete_account_opening_request}
+      end
     end
   end
 
   @spec get_or_open_account(Ecto.UUID.t(), map()) :: any()
   def get_or_open_account(user_id, account_opening_data) do
-    account_opening_data = transform_address_params(account_opening_data)
-
-    with {:ok, profile} <- Customers.upsert_profile(user_id, account_opening_data),
+    with {:ok, profile} <- Customers.upsert_profile_by_user_id(user_id, account_opening_data),
          {:ok, account_opening} <- request_account_opening(profile, account_opening_data),
-         {:ok, account_opening} <- maybe_complete_account_opening(account_opening, profile),
-         {:ok, referral_code} <- maybe_generate_referral_code(profile, account_opening) do
-      {:ok, Accounts.build_account(account_opening.status, referral_code.number)}
+         {:ok, account_opening} <- complete_account_opening(account_opening, profile),
+         {:ok, referral_code} <- generate_referral_code(profile, account_opening) do
+      {:ok, build_account(account_opening, profile, referral_code)}
     end
-  end
-
-  defp transform_address_params(%{} = account_opening_data) do
-    {address, account_opening_data} = Map.split(account_opening_data, [:city, :country, :state])
-    Map.merge(account_opening_data, %{address: address})
   end
 
   defp request_account_opening(profile, %{referral_code: referral_code})
        when not is_nil(referral_code) do
     with {:ok, referrer} <- Customers.get_referrer(referral_code) do
-      Accounts.request_account_opening(profile.id, referrer.id)
+      Accounts.get_or_create_account_opening_request(profile.id, referrer.id)
     end
   end
 
   defp request_account_opening(profile, _account_opening_data) do
-    Accounts.request_account_opening(profile.id)
+    Accounts.get_or_create_account_opening_request(profile.id)
   end
 
-  defp maybe_complete_account_opening(%{status: :complete} = account_opening, _profile) do
-    {:ok, account_opening}
-  end
-
-  defp maybe_complete_account_opening(account_opening, profile) do
-    if complete_account?(profile) do
-      Accounts.complete_account_opening(account_opening)
+  defp complete_account_opening(account_opening, profile) do
+    if Customers.complete_profile?(profile) do
+      Accounts.complete_account_opening_request(account_opening)
     else
       {:ok, account_opening}
     end
   end
 
-  defp complete_account?(profile) do
-    profile
-    |> Repo.preload(:address)
-    |> Customers.complete_profile_with_address?()
+  defp generate_referral_code(profile, account_opening_request) do
+    if Accounts.complete_account_opening_request?(account_opening_request) do
+      Customers.get_or_create_referral_code(profile.id)
+    else
+      {:ok, %{}}
+    end
   end
 
-  defp maybe_generate_referral_code(_profile, %{status: :pending}) do
-    {:ok, %Customers.ReferralCode{}}
-  end
-
-  defp maybe_generate_referral_code(profile, %{status: :complete}) do
-    Customers.get_or_create_referral_code(profile)
+  defp build_account(account_opening_request, profile, referral_code) do
+    account_opening_request
+    |> Map.from_struct()
+    |> Map.merge(%{
+      name: profile.name,
+      referral_code: Map.get(referral_code, :number)
+    })
   end
 end
--- test/magnemite_test.exs
+++ test/magnemite_test.exs
@@ -2,7 +2,31 @@ defmodule MagnemiteTest do
   use Magnemite.DataCase, async: true
 
   alias Magnemite
-  alias Magnemite.Customers
+  alias Magnemite.Customers.GenderOptions
+
+  describe "list_referred_accounts/1" do
+    test "" do
+      referrer = insert(:profile)
+      insert(:complete_account_opening_request, profile: referrer)
+
+      %{id: complete_account_opening_request1} =
+        insert(:complete_account_opening_request, referrer: referrer)
+
+      %{id: complete_account_opening_request2} =
+        insert(:complete_account_opening_request, referrer: referrer)
+
+      %{id: pending_account_opening_request} =
+        insert(:pending_account_opening_request, referrer: referrer)
+
+      assert {:ok, [_, _] = referred_accounts} = Magnemite.list_referred_accounts(referrer.user_id)
+
+      referred_accounts_ids = Enum.map(referred_accounts, & &1.id)
+
+      assert complete_account_opening_request1 in referred_accounts_ids
+      assert complete_account_opening_request2 in referred_accounts_ids
+      refute pending_account_opening_request in referred_accounts_ids
+    end
+  end
 
   describe "get_or_open_account/1" do
     @account_opening_data %{
@@ -11,7 +35,7 @@ defmodule MagnemiteTest do
       city: Faker.Address.PtBr.city(),
       country: Faker.Address.PtBr.country(),
       email: Faker.Internet.email(),
-      gender: [to_string(Enum.random(Customers.gender_options()))],
+      gender: [to_string(Enum.random(GenderOptions.list()))],
       name: Faker.Person.PtBr.name(),
       state: Faker.Address.PtBr.state()
     }
@@ -20,77 +44,82 @@ defmodule MagnemiteTest do
       user = insert(:user)
 
       %{
-        account_opening_data: %{
-          valid: Map.merge(@account_opening_data, %{user_id: user.id})
-        },
         user: user
       }
     end
 
     test """
-         returns a %Magnemite.Account{} with pending status and without referral code
+         returns an account with pending status and without referral code
          when given a new valid CPF, but not all necessary account opening data
          """,
          %{
-           account_opening_data: account_opening_data
+           user: user
          } do
-      valid_account_opening_data = Map.take(account_opening_data.valid, [:cpf, :user_id])
+      valid_account_opening_data = Map.take(@account_opening_data, [:cpf])
+
+      result = Magnemite.get_or_open_account(user.id, valid_account_opening_data)
 
       assert {:ok,
               %{
                 status: :pending,
                 referral_code: nil
-              }} = Magnemite.get_or_open_account(valid_account_opening_data)
+              }} = result
     end
 
     test """
-         returns a %Magnemite.Account{} with complete status and referral code
+         returns an account with complete status and referral code
          when given a new valid CPF and all necessary account opening data
          """,
          %{
-           account_opening_data: account_opening_data
+           user: user
          } do
+      result = Magnemite.get_or_open_account(user.id, @account_opening_data)
+
       assert {:ok,
               %{
-                status: :complete,
+                status: :done,
                 referral_code: referral_code
-              }} = Magnemite.get_or_open_account(account_opening_data.valid)
+              }} = result
 
       refute is_nil(referral_code)
     end
 
     test """
-         returns a %Magnemite.Account{} with pending status and without referral code
+         returns an account with pending status and without referral code
          when given a known valid CPF, but not all necessary account opening data
          """,
          %{
-           account_opening_data: account_opening_data
+           user: user
          } do
-      valid_account_opening_data =
-        Map.take(account_opening_data.valid, [:city, :cpf, :name, :user_id])
+      insert(:profile, cpf: @account_opening_data.cpf, user: user)
+
+      valid_account_opening_data = Map.take(@account_opening_data, [:city, :cpf, :email, :name])
+
+      result = Magnemite.get_or_open_account(user.id, valid_account_opening_data)
 
       assert {:ok,
               %{
                 status: :pending,
                 referral_code: nil
-              }} = Magnemite.get_or_open_account(valid_account_opening_data)
+              }} = result
     end
 
     test """
-         returns a %Magnemite.Account{} with complete status and referral code
+         returns an account with complete status and referral code
          when given a known valid CPF and all necessary account opening data
          """,
          %{
-           account_opening_data: account_opening_data,
            user: user
          } do
-      insert(:profile, cpf: account_opening_data.valid.cpf, user: user)
+      insert(:profile, cpf: @account_opening_data.cpf, user: user)
+
+      result = Magnemite.get_or_open_account(user.id, @account_opening_data)
 
       assert {:ok,
               %{
-                status: :complete,
+                status: :done,
                 referral_code: referral_code
-              }} = Magnemite.get_or_open_account(account_opening_data.valid)
+              }} = result
 
       refute is_nil(referral_code)
     end
